0551524230c7cbfd7c022d43f85102921c416056
jeroig <jeroig@gmail.com>
2021-03-16 12:28:40 -0300

    Add on Topic 'image' field for ActiveAdmin

--- app/admin/topics.rb
+++ app/admin/topics.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Topic do
   decorate_with TopicDecorator
 
-  permit_params :name
+  permit_params :name, :image
   # or
   #
   # permit_params do
@@ -10,13 +10,39 @@ ActiveAdmin.register Topic do
   #   permitted
   # end
 
+  # Documentation to support and ActiveStorage field in ActiveAdmin
+  # https://medium.com/@maris.cilitis/using-ruby-on-rails-active-storage-image-uploads-for-active-admin-backed-resources-5638a9ca0b46
+  form do |f|
+    f.inputs 'Details' do
+      f.input :name
+      f.input :image, as: :file,
+                      hint: f.object.image.attached? ? image_tag(f.object.image.variant(resize: '100x100')) : ''
+      # f.file_field :image, as: :file
+    end
+    actions
+  end
+
   index do
     selectable_column
     id_column
     column :name
+    column :image do |ad|
+      # image_tag url_for(ad.image) if ad.image.attached?
+      image_tag ad.image.variant(resize: '100x100') if ad.image.attached?
+    end
     column :created_at
     column :updated_at
 
     actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :image do |ad|
+        # image_tag url_for(ad.image) if ad.image.attached?
+        image_tag ad.image.variant(resize: '100x100') if ad.image.attached?
+      end
+    end
   end
 end
